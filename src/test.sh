80b3674493db1f85a1fcd48d0daf021fad759f23
Takuto Nanjo <16991946+taku-n@users.noreply.github.com>
2023-11-09 14:10:02 +0900

    test.sh

--- src/test.sh
+++ src/test.sh
@@ -3,9 +3,7 @@
 for x in $(find -name "*.md"); do
 	mainname=$(echo $x | sed 's/\.[^\.]*$//')
 	out=${mainname}.rs
-	sed "s/^/\/\/\/ /g" $x > $out
-	echo "#[allow(dead_code)]" >> $out  # Markdown files must have an empty line at the end
-	echo "fn f() {}" >> $out
+	sed "s/^/\/\/! /g" $x > $out
 done
 
 cargo test
